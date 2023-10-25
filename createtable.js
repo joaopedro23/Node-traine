e69178dc17a4e495d6160bd88cde99eaa607ae9f
joaopedro23 <joaopedro105manairapb@gmail.com>
2023-10-25 18:07:11 -0300

    API node com Postgres

---
+++ createtable.js
@@ -0,0 +1,16 @@
+import { sql } from "./dbsql.js";
+
+// sql`DROP TABLE IF EXISTS videos`.then(()=>{
+//     console.log("Table deleted");
+// })
+
+sql`
+    CREATE TABLE videos (
+        id TEXT PRIMARY KEY,
+        title TEXT,
+        description TEXT,
+        duration INTEGER
+    );
+`.then(()=>{ 
+    console.log("Table created");
+})
